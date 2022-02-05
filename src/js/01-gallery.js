e43798ac80a02991db656456ee1c037f0a6c42f9
EugeneKalinichenko <e.o.kalinichenko1994@gmail.com>
2022-02-05 20:59:25 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import galleryMarkupTpl from '../templates/gallery-markup.hbs';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.insertAdjacentHTML('beforeend', createGalleryItem(galleryItems));
+
+
+new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+  widthRatio: .9,
+});
+
+function createGalleryItem() { 
+  return galleryMarkupTpl(galleryItems);
+};
+
 
 console.log(galleryItems);
