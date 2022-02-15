15ea4e14e58606b9aa70f3f8d684f9dcbcdf73ac
EugeneKalinichenko <e.o.kalinichenko1994@gmail.com>
2022-02-15 20:40:47 +0200

    update task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,14 +2,19 @@ import throttle from "lodash.throttle";
 import '../css/03-feedback.css';
 import '../css/common.css';
 
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea')
+}
+
 const STORAGE_KEY = "feedback-form-state";
 
-const formData = {};
+let formData = {};
 
-const formEl = document.querySelector('.feedback-form');
-
-formEl.addEventListener('input', throttle (onFormInput, 500));
-formEl.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
 
 populateFormOutput();
 
@@ -20,20 +25,22 @@ function onFormInput(e) {
 };
 
 function onFormSubmit(e) { 
-  e.preventDefault();
+    e.preventDefault();
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
 
-  if (localStorage.getItem(STORAGE_KEY)) {
-    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
-  };
-  
-  e.currentTarget.reset();
-
-  localStorage.removeItem(STORAGE_KEY);
 };
 
-function populateFormOutput() { 
-  if (localStorage.getItem(STORAGE_KEY)) {
-    formEl.email.value = JSON.parse(localStorage.getItem(STORAGE_KEY)).email;
-    formEl.message.value = JSON.parse(localStorage.getItem(STORAGE_KEY)).message;
-  }
+function populateFormOutput() {
+    const saveMessage = localStorage.getItem(STORAGE_KEY)
+    const outputText = JSON.parse(saveMessage);
+    if (saveMessage) {
+        refs.email.value = outputText.email;
+        refs.textarea.value = outputText.message;
+    }
+    
+    formData = {
+        email: outputText.email,
+        message: outputText.message
+    }
 };
